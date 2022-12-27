d4056ff5d317fa6c652ef35144fd6017507af959
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-12-27 09:15:00 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { json } = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // using cors to resolve cors issues
 app.use(cors());
+app.use(json());
 
-app.listen(port, () => {
-  console.log("running on:", port);
+// localhost server setup
+const server = app.listen(port, "localhost", () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log(`🌐 Running at: http://${host}:${port}`);
 });
 
 // basic server setup
@@ -15,16 +24,29 @@ app.get("/", (req, res) => {
   res.send("Server running");
 });
 
-// courses data
-const courses = require("./data/courses/courses.json");
+// Connection URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_CLUSTER_URL}/?retryWrites=true&w=majority`;
 
-app.get("/courses", (req, res) => {
-  res.send(courses);
+// Create a new MongoClient
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1
 });
 
-// single course data
-app.get("/course/:id", (req, res) => {
-  const courseId = req.params.id;
-  const course = courses.find(course => course.id === courseId);
-  res.send(course);
-});
+async function run() {
+  try {
+    const db = client.db("devsDojo");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// // single course data
+// app.get("/course/:id", (req, res) => {
+//   const courseId = req.params.id;
+//   const course = courses.find(course => course.id === courseId);
+//   res.send(course);
+// });
