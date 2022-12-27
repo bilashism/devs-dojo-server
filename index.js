1c0c8d37b3709249bcdadb4e66b6abb236ec0b80
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-12-27 16:58:02 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const { json } = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// using cors to resolve cors issues
+// using middleware
 app.use(cors());
 app.use(json());
 
@@ -24,29 +23,6 @@ app.get("/", (req, res) => {
   res.send("Server running");
 });
 
-// Connection URI
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_CLUSTER_URL}/?retryWrites=true&w=majority`;
-
-// Create a new MongoClient
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1
-});
-
-async function run() {
-  try {
-    const db = client.db("devsDojo");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
-
-// // single course data
-// app.get("/course/:id", (req, res) => {
-//   const courseId = req.params.id;
-//   const course = courses.find(course => course.id === courseId);
-//   res.send(course);
-// });
+// for courses route
+const courses = require("./routes/courses");
+app.use("/courses", courses);
---
+++ utilities/mongodb.js
@@ -0,0 +1,25 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+// Connection URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_CLUSTER_URL}/?retryWrites=true&w=majority`;
+
+/* Creating a new MongoClient object. */
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1
+});
+
+/**
+ * It connects to the database
+ */
+const connectToDb = async () => await client.connect();
+/* Creating a new database object. */
+const db = client.db(`${process.env.DB_NAME}`);
+/**
+ * It closes the connection to the database.
+ */
+const closeDbConnection = async () => await client.close();
+
+/* Exporting the functions to be used in other files. */
+module.exports = { connectToDb, db, closeDbConnection, ObjectId };
